def61928be547a42245b7ce8a83f8823d89acd81
ramishra <ramishra@BLR2DEVLP0431.misys.global.ad>
2017-05-17 15:39:39 +0530

    1st Version to GitHub

---
+++ TestProjectForGitDemo/src/com/misys/welcome/WelcomeMessage.java
@@ -0,0 +1,12 @@
+package com.misys.welcome;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World.");
+		System.out.println("Hi");
+		System.out.println("What a place Misys is!!");
+	}
+
+}
