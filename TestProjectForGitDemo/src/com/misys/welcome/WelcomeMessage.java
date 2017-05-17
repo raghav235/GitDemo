1af1d3536b845e5a6fc4cea68fafa7e771e177bd
ramishra <ramishra@BLR2DEVLP0431.misys.global.ad>
2017-05-17 15:48:00 +0530

    Version 2 new Syso Added.

--- TestProjectForGitDemo/src/com/misys/welcome/WelcomeMessage.java
+++ TestProjectForGitDemo/src/com/misys/welcome/WelcomeMessage.java
@@ -7,6 +7,8 @@ public class WelcomeMessage {
 		System.out.println("Hello World.");
 		System.out.println("Hi");
 		System.out.println("What a place Misys is!!");
+		
+		System.out.println("Welcome To new World of Lending..");
 	}
 
 }
